aa4dc22d95440970861282256cf7ca110c3a0257
automation <testesautomacao2021@gmail.com>
2021-09-09 11:13:45 -0300

    alt request formParam

--- src/test/java/models/datasets/BodyCreateDatasets.java
+++ src/test/java/models/datasets/BodyCreateDatasets.java
@@ -13,11 +13,17 @@ public class BodyCreateDatasets {
     @JsonIgnore
     private Faker faker;
     private String name;
+    private String experimentId;
     private File file;
 
     public BodyCreateDatasets() {
         this.faker = new Faker();
         this.name  = faker.lorem().characters(9);
+        this.experimentId  = faker.lorem().characters(8) +
+                "-" + faker.lorem().characters(4) +
+                "-"+ faker.lorem().characters(4) +
+                "-"+ faker.lorem().characters(4) +
+                "-"+ faker.lorem().characters(12);
 
     }
 }
