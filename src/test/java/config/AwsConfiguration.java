282a6e3398506fcff50a8e0d30b0c909491e929b
testesAutomation <testesautomacao2021@gmail.com>
2022-01-26 21:18:04 -0300

    models aws env

---
+++ src/test/java/config/AwsConfiguration.java
@@ -0,0 +1,13 @@
+package config;
+
+public class AwsConfiguration {
+
+    public void teste(){
+        System.getenv("AWS_ACESS_KEY_ID");
+        System.getenv("AWS_SECRET_ACESS_KEY");
+        System.getenv("KMS_ARM");
+        System.getenv("REGION");
+
+
+    }
+}
